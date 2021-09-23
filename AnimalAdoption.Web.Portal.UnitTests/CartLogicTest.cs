c3868a0472125d5e09584fcc27e5b1f6daed6286
Catherine Siriett <catherine.siriett@awaregroup.com>
2021-09-24 08:59:58 +1200

    Fix test issues.

--- AnimalAdoption.Web.Portal.UnitTests/CartLogicTest.cs
+++ AnimalAdoption.Web.Portal.UnitTests/CartLogicTest.cs
@@ -1,4 +1,6 @@
+using AnimalAdoption.Common.Domain;
 using AnimalAdoption.Common.Logic;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Caching.Memory;
 using System.Linq;
 using Xunit;
@@ -7,30 +9,44 @@ namespace AnimalAdoption.Web.Portal.UnitTests
 {
     public class CartLogicTest
     {
+        private DbContextOptions<AnimalAdoptionContext> _options = new DbContextOptionsBuilder<AnimalAdoptionContext>()
+                     .UseInMemoryDatabase(databaseName: "AnimalDatabase")
+                     .Options;
+
         [Fact]
         public void CartManagement_EmptyCartAddAnimal_AnAnimalIsAdded()
         {
-            var animalId = 1;
-            var quantityAmount = 1;
+            using (var context = new AnimalAdoptionContext(_options))
+            {
+                DbInitializer.Initialize(context);
+                var animalId = 1;
+                var quantityAmount = 1;
 
-            var memoryCache = new MemoryCache(new MemoryCacheOptions());
-            var resultingCart = new CartService(memoryCache, new AnimalService()).SetAnimalQuantity("TEST_CART", animalId, quantityAmount);
+                var memoryCache = new MemoryCache(new MemoryCacheOptions());
+                var animalService = new AnimalService(context);
+                var resultingCart = new CartService(memoryCache, animalService).SetAnimalQuantity("TEST_CART", animalId, quantityAmount);
 
-            Assert.Equal("TEST_CART", resultingCart.Id);
-            Assert.Equal(1, resultingCart.CartContents.First(x => x.Id == animalId).Quantity);
+                Assert.Equal("TEST_CART", resultingCart.Id);
+                Assert.Equal(1, resultingCart.CartContents.First(x => x.Id == animalId).Quantity);
+            }
         }
 
         [Fact]
         public void CartManagement_EmptyCartAddNegativeAnimal_AnAnimalDoesNotGoIntoNegative()
         {
-            var animalId = 1;
-            var quantityAmount = -1;
+            using (var context = new AnimalAdoptionContext(_options))
+            {
+                DbInitializer.Initialize(context);
+                var animalId = 1;
+                var quantityAmount = -1;
 
-            var memoryCache = new MemoryCache(new MemoryCacheOptions());
-            var resultingCart = new CartService(memoryCache, new AnimalService()).SetAnimalQuantity("TEST_CART", animalId, quantityAmount);
+                var memoryCache = new MemoryCache(new MemoryCacheOptions());
+                var animalService = new AnimalService(context);
+                var resultingCart = new CartService(memoryCache, animalService).SetAnimalQuantity("TEST_CART", animalId, quantityAmount);
 
-            Assert.Equal("TEST_CART", resultingCart.Id);
-            Assert.Equal(0, resultingCart.CartContents.First(x => x.Id == animalId).Quantity);
+                Assert.Equal("TEST_CART", resultingCart.Id);
+                Assert.Equal(0, resultingCart.CartContents.First(x => x.Id == animalId).Quantity);
+            }
         }
     }
 }
