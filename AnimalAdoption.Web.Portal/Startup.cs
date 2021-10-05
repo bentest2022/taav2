e535ec9a3784335b99dc128fc16d3432fa4526e9
Chryogenic <b@en.vc>
2021-10-05 16:14:34 +1300

    Added check for connection string variable

--- AnimalAdoption.Web.Portal/Startup.cs
+++ AnimalAdoption.Web.Portal/Startup.cs
@@ -15,92 +15,97 @@ using System.Threading.Tasks;
 
 namespace AnimalAdoption.Web.Portal
 {
-    public class Startup
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-        private string _connectionString;
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.Configure<Configuration>(Configuration);
-
-            _connectionString = "YOUR CONNECTIONS STRING";
-
-            services.AddDbContext<AnimalAdoptionContext>(options =>
-            {
-                options.UseSqlServer(_connectionString);
-            });
-
-            services.AddHttpContextAccessor();
-            services.AddMemoryCache();
-            services.AddTransient<AnimalService>();
-            services.AddRazorPages();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                app.UseHttpsRedirection();
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
-            app.UseStaticFiles();
-
-            var failurePercentage = Configuration.GetValue<int?>("SimulatedFailureChance");
-            if (failurePercentage != null)
-            {
-                var rand = new Random();
-                app.Use(async (context, next) =>
-                {
-                    if (failurePercentage <= rand.Next(0, 100))
-                    {
-                        await next.Invoke();
-                    }
-                    else
-                    {
-                        throw new Exception($"A simulated failure occurred - there is a {failurePercentage}% chance of this occurring");
-                    }
-                });
-            }
-
-            if (string.IsNullOrWhiteSpace(_connectionString))
-            {
-                app.Use(async (context, next) =>
-                {
-                    var url = context.Request.Path.Value;
-                    if (!url.ToLowerInvariant().Contains("/missingenvironmentvariable"))
-                    {
-                        // rewrite and continue processing
-                        context.Request.Path = "/missingenvironmentvariable";
-                    }
-
-                    await next();
-                });
-            }
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapRazorPages();
-                endpoints.MapControllers();
-            });
-        }
+      Configuration = configuration;
     }
+
+    public IConfiguration Configuration { get; }
+    private string _connectionString;
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.Configure<Configuration>(Configuration);
+
+      _connectionString = ""; //SET YOUR SQL DB CONNECTION STRING HERE
+
+      if (string.IsNullOrEmpty(_connectionString))
+      {
+        throw new Exception("Please make sure that you have set a SQL DB connection string...");
+      }
+
+      services.AddDbContext<AnimalAdoptionContext>(options =>
+      {
+        options.UseSqlServer(_connectionString);
+      });
+
+      services.AddHttpContextAccessor();
+      services.AddMemoryCache();
+      services.AddTransient<AnimalService>();
+      services.AddRazorPages();
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+      else
+      {
+        app.UseExceptionHandler("/Error");
+        app.UseHttpsRedirection();
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+      }
+
+      app.UseStaticFiles();
+
+      var failurePercentage = Configuration.GetValue<int?>("SimulatedFailureChance");
+      if (failurePercentage != null)
+      {
+        var rand = new Random();
+        app.Use(async (context, next) =>
+        {
+          if (failurePercentage <= rand.Next(0, 100))
+          {
+            await next.Invoke();
+          }
+          else
+          {
+            throw new Exception($"A simulated failure occurred - there is a {failurePercentage}% chance of this occurring");
+          }
+        });
+      }
+
+      if (string.IsNullOrWhiteSpace(_connectionString))
+      {
+        app.Use(async (context, next) =>
+        {
+          var url = context.Request.Path.Value;
+          if (!url.ToLowerInvariant().Contains("/missingenvironmentvariable"))
+          {
+            // rewrite and continue processing
+            context.Request.Path = "/missingenvironmentvariable";
+          }
+
+          await next();
+        });
+      }
+
+      app.UseRouting();
+
+      app.UseAuthorization();
+
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapRazorPages();
+        endpoints.MapControllers();
+      });
+    }
+  }
 }
